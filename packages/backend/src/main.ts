1cef034e47da0526ae15fa9cb78c054a0215ef19
Inosensey <dingcong.bae@gmail.com>
2026-02-11 21:32:37 +0800

    Configure global middleware: cookieParser, pipes, and filters

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
+import { PrismaExceptionFilter } from './PrismaExceptionFilter';
+import { HttpExceptionFilter } from './HttpExceptionFilter';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new PrismaExceptionFilter(), new HttpExceptionFilter());
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
