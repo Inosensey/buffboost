9b8bfef085379a85821d7a3ea8c1fe8378aa979c
Inosensey <dingcong.bae@gmail.com>
2026-02-19 17:45:08 +0800

    Register UserSessionService in user module providers

--- packages/backend/src/modules/userModule/user.module.ts
+++ packages/backend/src/modules/userModule/user.module.ts
@@ -2,10 +2,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserSessionService } from '../userSessionModule/userSession.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserSessionService],
   exports: [UserService],
 })
 export class UserModule {}
