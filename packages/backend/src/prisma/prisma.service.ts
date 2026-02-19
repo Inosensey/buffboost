87955d4faaba18570b7f19247536b74d4a0ebce5
Philip Mathew <76528638+Inosensey@users.noreply.github.com>
2026-02-19 18:35:24 +0800

    Move dotenv.config() call into constructor

--- packages/backend/src/prisma/prisma.service.ts
+++ packages/backend/src/prisma/prisma.service.ts
@@ -2,14 +2,13 @@ import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import * as dotenv from 'dotenv';
 
-dotenv.config();
-
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
+    dotenv.config();
     super({
       datasourceUrl: process.env.DATABASE_URL,
     });
