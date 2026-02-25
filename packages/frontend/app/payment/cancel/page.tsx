58be4e3158f125bf70cb2d4c1038b9b3ff82bcbb
Inosensey <dingcong.bae@gmail.com>
2026-02-25 10:21:55 +0800

    Add Suspense to the cancel page.

--- packages/frontend/app/payment/cancel/page.tsx
+++ packages/frontend/app/payment/cancel/page.tsx
@@ -1,12 +1,15 @@
 "use server";
 
 import CancelPaymentContent from "@/components/paymentComponents/CancelPaymentContent";
+import { Suspense } from "react";
 
 const CancelPaymentPage = async () => {
   return (
-    <div className="flex items-center justify-center font-sans">
-      <CancelPaymentContent />
-    </div>
+    <Suspense fallback={<div>Loading payment details...</div>}>
+      <div className="flex items-center justify-center font-sans">
+        <CancelPaymentContent />
+      </div>
+    </Suspense>
   );
 };
 
