278ff2833331ae740b4969f7d04ebaef34c29a37
Inosensey <dingcong.bae@gmail.com>
2026-03-19 18:36:51 +0800

    Fetch payment data from NestJS API

--- packages/frontend/app/payment/success/page.tsx
+++ packages/frontend/app/payment/success/page.tsx
@@ -1,13 +1,27 @@
 "use server";
 
 import { Suspense } from "react";
+import { cookies } from "next/headers";
+
+// lib/Api
+import { nestJsEndpoints } from "@/lib/nestJsEndpoints";
+
 import SuccessPaymentContent from "@/components/paymentComponents/SuccessPaymentContent";
 
-const SuccessPaymentPage = async () => {
+// Types
+interface props {
+  searchParams: Promise<{ session_id: string }>;
+}
+
+const SuccessPaymentPage = async ({ searchParams }: props) => {
+  const params = await searchParams;
+
+  const response = await nestJsEndpoints.buffsApi.getPurchasedBuffs(params.session_id);
+
   return (
     <Suspense fallback={<div>Loading payment details...</div>}>
       <div className="flex items-center justify-center font-sans">
-        <SuccessPaymentContent />
+        <SuccessPaymentContent purchaseBuffs={response.data} />
       </div>
     </Suspense>
   );
