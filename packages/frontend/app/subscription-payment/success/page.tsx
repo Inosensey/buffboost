254b42c40d71baf021cefb77ee8ccb1bd8bad459
Inosensey <dingcong.bae@gmail.com>
2026-03-19 18:38:31 +0800

    Rename subscription payment pages and integrate API data

--- packages/frontend/app/subscription-payment/success/page.tsx
+++ packages/frontend/app/subscription-payment/success/page.tsx
@@ -1,16 +1,29 @@
 "use server";
 
 import SuccessPaymentContent from "@/components/subscriptionPaymentComponents/SuccessPaymentContent";
+import { nestJsEndpoints } from "@/lib/nestJsEndpoints";
 import { Suspense } from "react";
 
-const SuccessPaymentPage = async () => {
+
+// Types
+interface props {
+  searchParams: Promise<{ session_id: string }>;
+}
+
+const SubscriptionSuccessPaymentPage = async ({ searchParams }: props) => {
+  const params = await searchParams;
+
+  
+    const response = await nestJsEndpoints.buffsApi.getCurrentActiveBuffBySessionId(params.session_id);
+    const activeBuff = response.data;
+
   return (
     <Suspense fallback={<div>Loading payment details...</div>}>
       <div className="flex items-center justify-center font-sans">
-        <SuccessPaymentContent />
+        <SuccessPaymentContent activeBuff={activeBuff} />
       </div>
     </Suspense>
   );
 };
 
-export default SuccessPaymentPage;
+export default SubscriptionSuccessPaymentPage;
