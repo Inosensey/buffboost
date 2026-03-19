7d20cfe054146c0f3ae00849df288199d4d786c7
Inosensey <dingcong.bae@gmail.com>
2026-03-19 18:34:54 +0800

    Create InstantBuffs and DailyBlessings components for buff display

---
+++ packages/frontend/components/boostingStationComponents/DailyBlessings.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import BuffCard from "./BuffCard";
+
+// types
+interface props {
+    buffList: Buff[];
+    currentActiveBuff: ActiveBuff | null;
+    selectedBuffs: Buff[];
+    handleSelectBuff: (buff: Buff) => void;
+}
+
+// Constants
+const MAX_SELECTIONS = 1;
+
+const DailyBlessings = ({ buffList, currentActiveBuff, selectedBuffs, handleSelectBuff }: props) => {
+  return (
+    <div className="mt-2 py-1 overflow-hidden h-[85%] flex flex-wrap gap-3 justify-center">
+      {buffList.map((buff: Buff) => (
+        <BuffCard
+          key={buff.id}
+          isPurchased={currentActiveBuff
+              ? true
+              : false
+          }
+          buffInfo={buff}
+          isSelected={
+            selectedBuffs.find((selectedBuff) => selectedBuff.id === buff.id)
+              ? true
+              : false
+          }
+          onSelect={handleSelectBuff}
+          maxSelections={MAX_SELECTIONS}
+          currentSelections={selectedBuffs.length + (currentActiveBuff ? 1 : 0)}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default DailyBlessings
