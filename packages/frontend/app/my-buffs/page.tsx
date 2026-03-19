23f0a6bd4820577fd6aba2f16ac8091c63280fd3
Inosensey <dingcong.bae@gmail.com>
2026-03-19 18:36:29 +0800

    Fetch buffs from NestJS API for myBuffs page

--- packages/frontend/app/my-buffs/page.tsx
+++ packages/frontend/app/my-buffs/page.tsx
@@ -1,11 +1,17 @@
 "use server"
 
+import { nestJsEndpoints } from "@/lib/nestJsEndpoints";
+
 import MyBuffsContent from "@/components/myBuffsComponents/MyBuffsContent";
 
 const MyBuffsPage = async () => {
+
+  const purchasedBuffsTodayResponse = await nestJsEndpoints.buffsApi.getCurrentPurchasedBuffsToday(); 
+  const purchasedBuffsHistoryResponse = await nestJsEndpoints.buffsApi.getCurrentPurchasedBuffsHistory();
+
   return (
     <div className="flex items-center justify-center font-sans">
-      <MyBuffsContent />
+      <MyBuffsContent historyBuffs={purchasedBuffsHistoryResponse.data} todayBuffs={purchasedBuffsTodayResponse.data} />
     </div>
   );
 };
