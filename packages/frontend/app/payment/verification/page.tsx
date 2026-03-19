ed8602d4601034b952987281282f40672ffc799d
Inosensey <dingcong.bae@gmail.com>
2026-03-19 18:36:51 +0800

    Fetch verification data from NestJS API

--- packages/frontend/app/payment/verification/page.tsx
+++ packages/frontend/app/payment/verification/page.tsx
@@ -1,9 +1,31 @@
 "use server";
 
-import VerifyPaymentContent from "@/components/paymentComponents/VerifyPaymentContent";
 import { Suspense } from "react";
+import { cookies } from "next/headers";
 
-const VerifyPage = async () => {
+// lib/Api
+import { nestJsEndpoints } from "@/lib/nestJsEndpoints";
+
+// Components
+import VerifyPaymentContent from "@/components/paymentComponents/VerifyPaymentContent";
+
+
+// Types
+interface props {
+  searchParams: Promise<{ session_id: string }>;
+}
+const VerifyPage = async ({ searchParams }: props) => {
+  // Get cookies from the incoming request
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
+  
+  nestJsEndpoints.setServerToken(token || null);
+
+  const params = await searchParams;
+
+  const response = await nestJsEndpoints.buffsApi.getPurchasedBuffs(params.session_id);
+  
+  console.log(response);
   return (
     <Suspense fallback={<div>Loading payment details...</div>}>
       <div className="flex items-center justify-center font-sans">
