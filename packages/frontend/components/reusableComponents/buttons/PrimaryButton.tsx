92767e11dfbb464820ff570ceb06712104a5b215
Inosensey <dingcong.bae@gmail.com>
2026-03-19 18:35:30 +0800

    Add onClick prop to PrimaryButton

--- packages/frontend/components/reusableComponents/buttons/PrimaryButton.tsx
+++ packages/frontend/components/reusableComponents/buttons/PrimaryButton.tsx
@@ -7,13 +7,15 @@ interface PrimaryButtonProps {
   type: "submit" | "reset" | "button";
   buttonName: string;
   Icon: React.ComponentType<{ color: string; width?: string; height?: string }>;
+  onClickFn?: () => void;
 }
 
-const PrimaryButton = ({ type, buttonName, Icon }: PrimaryButtonProps) => {
+const PrimaryButton = ({ type, buttonName, Icon, onClickFn }: PrimaryButtonProps) => {
   return (
     <button
       className="flex gap-1 items-center bg-Primary rounded-lg py-1 px-3 cursor-pointer font-semibold font-oxanium phone:text-sm mdtablet:text-base hover:scale-[1.1] active:scale-[0.9] active:bg-Background transition-all duration-200 "
       type={type}
+      onClick={onClickFn}
     >
       {buttonName}
       <Icon width="1.2em" height="1.2em" color="#fff" />
