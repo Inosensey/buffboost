712162520afdbabf1437003e47647d518951b942
Inosensey <dingcong.bae@gmail.com>
2026-04-17 09:41:59 +0800

    Integrate active buff API with myBuffs page
    - Call getCurrentActiveBuff API
    - Pass active buff data to MyBuffsContent as props

--- packages/frontend/app/my-buffs/page.tsx
+++ packages/frontend/app/my-buffs/page.tsx
@@ -1,17 +1,24 @@
-"use server"
+"use server";
 
 import { nestJsEndpoints } from "@/lib/nestJsEndpoints";
 
 import MyBuffsContent from "@/components/myBuffsComponents/MyBuffsContent";
 
 const MyBuffsPage = async () => {
-
-  const purchasedBuffsTodayResponse = await nestJsEndpoints.buffsApi.getCurrentPurchasedBuffsToday(); 
-  const purchasedBuffsHistoryResponse = await nestJsEndpoints.buffsApi.getCurrentPurchasedBuffsHistory();
+  const purchasedBuffsTodayResponse =
+    await nestJsEndpoints.buffsApi.getCurrentPurchasedBuffsToday();
+  const currentActiveBuff =
+    await nestJsEndpoints.buffsApi.getCurrentActiveBuff();
+  const purchasedBuffsHistoryResponse =
+    await nestJsEndpoints.buffsApi.getCurrentPurchasedBuffsHistory();
 
   return (
     <div className="flex items-center justify-center font-sans">
-      <MyBuffsContent historyBuffs={purchasedBuffsHistoryResponse.data} todayBuffs={purchasedBuffsTodayResponse.data} />
+      <MyBuffsContent
+        historyBuffs={purchasedBuffsHistoryResponse.data}
+        todayBuffs={purchasedBuffsTodayResponse.data}
+        buffSubscription={currentActiveBuff.data}
+      />
     </div>
   );
 };
