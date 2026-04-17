7054dc7ecd894946c9c1bb4f44e379e95e650aa9
Inosensey <dingcong.bae@gmail.com>
2026-04-17 09:43:47 +0800

    Adjust clickEvent prop handling

--- packages/frontend/components/reusableComponents/Overlay.tsx
+++ packages/frontend/components/reusableComponents/Overlay.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 import { motion } from "framer-motion";
 
 interface props {
-  clickEvent: () => void;
+  clickEvent?: () => void;
   children?: ReactNode;
 }
 
