2f92915e4488d7c761b7432472642c9333c38315
Inosensey <dingcong.bae@gmail.com>
2026-04-17 09:41:51 +0800

    Add cache revalidation and subscription cancellation
    - Add updateTag for cache revalidation in createStripeCheckoutSession
    - Add cancelSubscription method

--- packages/frontend/actions/paymentActions.ts
+++ packages/frontend/actions/paymentActions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { nestJsEndpoints } from "@/lib/nestJsEndpoints";
+import { updateTag } from "next/cache";
 import { cookies } from "next/headers";
 
 export const createStripeCheckoutSession = async (
@@ -10,19 +11,26 @@ export const createStripeCheckoutSession = async (
   const checkoutType = formData.get("checkoutType") as string;
   const checkoutItemsJson = formData.get("checkoutItems") as string;
   const checkoutItemsPayload = JSON.parse(checkoutItemsJson) as checkoutItem[];
+  const cookieStore = await cookies();
 
   try {
+    const userId = cookieStore.get("user_id")?.value;
     let response;
     if (checkoutType === "instantBuffs") {
       response =
         await nestJsEndpoints.stripesApi.createCheckoutSession(
           checkoutItemsPayload,
         );
+
+      updateTag("buffs");
+      updateTag(`purchasedBuffsToday-${userId}`);
+      updateTag(`purchasedBuffsHistory-${userId}`);
     } else {
       response =
         await nestJsEndpoints.stripesApi.createCheckoutSubscriptionSession(
           checkoutItemsPayload,
         );
+      updateTag(`activeBuff-${userId}`);
     }
 
     if (!response.success) {
@@ -33,14 +41,41 @@ export const createStripeCheckoutSession = async (
       };
     }
 
-    console.log(response);
-
     return response;
   } catch (error) {
     console.log(error);
     const errorMessage: string =
       error instanceof Error
         ? `There is an error Creating Checkout Session: ${error.message}`
+        : "An unknown error occurred";
+    return {
+      success: false,
+      data: null,
+      message: errorMessage,
+    };
+  }
+};
+
+export const cancelSubscription = async (
+  prevState: ApiResponse<any | null>,
+  formData: FormData,
+): Promise<ApiResponse<any | null>> => {
+  const cookieStore = await cookies();
+  try {
+    const userId = cookieStore.get("user_id")?.value;
+
+    console.log(userId);
+    // revalidateTag("")
+    return {
+      success: true,
+      data: null,
+      message: ``,
+    };
+  } catch (error) {
+    console.log(error);
+    const errorMessage: string =
+      error instanceof Error
+        ? `There is an error Canceling Subscription: ${error.message}`
         : "An unknown error occurred";
     return {
       success: false,
